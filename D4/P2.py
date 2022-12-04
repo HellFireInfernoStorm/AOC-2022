6ccbb03c333e2c16a27999db7ddc7fd828b4bb12
HellFireInfernoStorm <dinura2003@gmail.com>
2022-12-04 11:06:06 +0530

    Day 4

---
+++ D4/P2.py
@@ -0,0 +1,16 @@
+data = []
+count = 0
+
+with open('input.txt') as f:
+    for line in f:
+        line = [[int(n) for n in pair.split('-')] for pair in line.strip().split(',')]
+        data.append(line)
+
+for line in data:
+    a, b = line[0][0], line[0][1]
+    c, d = line[1][0], line[1][1]
+    if a>d or c>b:
+        continue
+    count += 1
+
+print(count)
