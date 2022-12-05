ef08e710cbdaccc585c753649d2cbfe0143b4bf3
HellFireInfernoStorm <dinura2003@gmail.com>
2022-12-05 12:51:57 +0530

    Day 5

---
+++ D5/P1.py
@@ -0,0 +1,25 @@
+with open('input.txt') as f:
+    data = f.read().splitlines()
+
+stacks = {i : [] for i in range(1,10)}
+steps = []
+
+for i in range(7,-1,-1):
+    for j in range(1,10):
+        c = data[i][j*4-3]
+        if c != ' ':
+            stacks[j].append(c)
+
+for line in data[10:]:
+    i = line.find('from')
+    n = int(line[5:i-1])
+    a = int(line[i+5])
+    b = int(line[i+10])
+    for i in range(n):
+        stacks[b].append(stacks[a].pop())
+
+word = ''
+for i in range(1,10):
+    word += stacks[i].pop()
+
+print(word)
---
+++ D5/P2.py
@@ -0,0 +1,25 @@
+with open('input.txt') as f:
+    data = f.read().splitlines()
+
+stacks = {i : [] for i in range(1,10)}
+steps = []
+
+for i in range(7,-1,-1):
+    for j in range(1,10):
+        c = data[i][j*4-3]
+        if c != ' ':
+            stacks[j].append(c)
+
+for line in data[10:]:
+    offset = line.find('from')
+    n = int(line[5:offset-1])
+    a = int(line[offset+5])
+    b = int(line[offset+10])
+    stacks[b].extend(stacks[a][-n::])
+    stacks[a] = stacks[a][:-n]
+
+word = ''
+for i in range(1,10):
+    word += stacks[i].pop()
+
+print(word)
